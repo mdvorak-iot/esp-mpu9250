644532663f8b5839f01319c0628e8dd31bdb1101
Michal Dvorak <mikee2185@gmail.com>
2021-12-26 16:16:54 +0100

    fix(i2c): initialize i2c_config_t conf
    It fails on current ESP-IDF otherwise, since new fields contain garbage.

--- components/mpu9250/i2c-easy.c
+++ components/mpu9250/i2c-easy.c
@@ -48,7 +48,7 @@
 esp_err_t i2c_master_init(uint8_t i2c_num, uint8_t gpio_sda, uint8_t gpio_scl)
 {
   int i2c_master_port = i2c_num;
-  i2c_config_t conf;
+  i2c_config_t conf = {};
   conf.mode = I2C_MODE_MASTER;
   conf.sda_io_num = gpio_sda;
   conf.sda_pullup_en = 0;
